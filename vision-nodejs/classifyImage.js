ea34ee847d81fb2f68af903399cdcc799ad4128d
Sonam Ghosh <sonamghosh@gmail.com>
2019-04-14 01:20:01 -0400

    Put a directory creating method

--- vision-nodejs/classifyImage.js
+++ vision-nodejs/classifyImage.js
@@ -1,24 +1,8 @@
-// api key = 'AIzaSyBEXYUNUaFk36Ub6jw1tKK8CjpYagP34vA'
 
-// Import google cloud client library
-const vision = require('@google-cloud/vision');
 
-// Create a client
-const client = new vision.ImageAnnotatorClient();
-
-// Perform a label detection on a image file
-client.labelDetection('corgi.png').then(results => {
-  const labels = results[0].labelAnnotations;
-
-  console.log('Labels:');
-  labels.forEach(label => console.log(label.description));
-
-}).catch(err => {
-  console.err('ERROR', err);
-});
-
-// Method
+// Function to classify
 async function imageClassify(filename) {
+  // Import google cloud client library
   const vision = require('@google-cloud/vision');
 
   // Create a client
@@ -30,5 +14,24 @@ async function imageClassify(filename) {
   labels.forEach(label => console.log(label.description));
 }
 
+// Test
 const f = 'corgi.png'
 imageClassify(f);
+
+
+// Create directory
+function createDir(folderName) {
+  const fs = require('fs');
+
+  try {
+    if (!fs.existsSync(folderName)){
+      fs.mkdirSync(folderName)
+    }
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+// Test
+const f2 = 'test2'
+createDir(f2);
